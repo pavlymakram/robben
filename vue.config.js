a35855af3722c57b322ecd97115e43681496300d
pavlymakram <pavlymakram2@gmail.com>
2024-10-26 18:09:30 +0300

    pavly

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    transpileDependencies: [
+      'dependency-name', // Replace with the dependencies you need to transpile
+      // add other dependencies as needed
+    ],
+  }
+  
